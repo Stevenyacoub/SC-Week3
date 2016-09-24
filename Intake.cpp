4e822bbe7d17be19cb7e23dfb693a38006924fed
danielhr444 <danielhr444@gmail.com>
2016-09-23 22:29:53 -0700

    intakeCode

---
+++ Intake.cpp
@@ -0,0 +1,18 @@
+/*
+ * Intake.cpp
+ *
+ *  Created on: Sep 17, 2016
+ *      Author: danie
+ */
+
+#include "Intake.h"
+
+Intake::Intake() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Intake::~Intake() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Intake.h
@@ -0,0 +1,18 @@
+/*
+ * Intake.h
+ *
+ *  Created on: Sep 17, 2016
+ *      Author: danie
+ */
+
+#ifndef INTAKE_H_
+#define INTAKE_H_
+
+class Intake {
+public:
+	Intake();
+	virtual ~Intake();
+};
+
+#endif /* INTAKE_H_ */
+

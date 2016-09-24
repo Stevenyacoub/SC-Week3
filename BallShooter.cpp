2ac7c18af46780d4b3094c1b4de1061431c9f1a6
Stevenyacoub <yacoubsteven@gmail.com>
2016-09-24 00:14:58 -0700

    This should update ballshooter

--- BallShooter.cpp
+++ BallShooter.cpp
@@ -4,7 +4,17 @@
  *  Created on: Sep 17, 2016
  *      Author: Steven
  */
+int(main);
+1,2 * Shooter.cpp;
+3,4 * Intake.cpp;
 
-
-
-
+bool intakeBall = intakeBall();
+if intakeBall() = true{
+	std::cout("Ball shot")std::endl;
+}
+if intakeBall() = false{
+	std::cout("no ball returned")std::endl;
+}
+delete Shooter.cpp;
+delete Intake.cpp;
+return 0;
